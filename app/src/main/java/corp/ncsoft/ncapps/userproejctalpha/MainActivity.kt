29cf7879a25faf5a6405fc83284e55d6ed42c58a
dongsik kim <simsooltong@gmail.com>
2020-06-03 13:34:27 +0900

    common 모듈의 dependency를 공유하여 로티 실행

--- app/src/main/java/corp/ncsoft/ncapps/userproejctalpha/MainActivity.kt
+++ app/src/main/java/corp/ncsoft/ncapps/userproejctalpha/MainActivity.kt
@@ -2,10 +2,16 @@ package corp.ncsoft.ncapps.userproejctalpha
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.airbnb.lottie.LottieAnimationView
+import corp.ncsoft.ncapps.common.ui.loadDefaultLottie
 
 class MainActivity : AppCompatActivity() {
+    lateinit var lottieAnimView: LottieAnimationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lottieAnimView = findViewById(R.id.lottie_anim)
+        findViewById<AppCompatButton>(R.id.test_button).setOnClickListener { lottieAnimView.loadDefaultLottie() }
     }
 }
